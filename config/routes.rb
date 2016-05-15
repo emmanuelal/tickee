965bd4a6a96be0e871ff4a09f36ed58b30d5b999
emmanuelal <emmanuelalcime54@gmail.com>
2016-05-15 15:59:38 -0400

    Only admins can create or delete projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
-  get 'application/index'
+     get 'application/index'
+     resources :projects, only: [:new, :create, :destroy]
   end
 
   devise_for :users
@@ -9,9 +10,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'projects#index'
-  resources :projects do
-    resources :tickets
-  end  
+  
+  resources :projects, only: [:index, :show, :edit, :update] do
+        resources :tickets
+   end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/controllers/admin/application_controller_spec.rb
+++ spec/controllers/admin/application_controller_spec.rb
@@ -1,12 +1,17 @@
-require 'rails_helper'
+require "rails_helper"
+    RSpec.describe Admin::ApplicationController, type: :controller do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+         
+         allow(controller).to receive(:authenticate_user!)
+         allow(controller).to receive(:current_user).and_return(user)
 
-RSpec.describe Admin::ApplicationController, type: :controller do
-
-  describe "GET #index" do
-    it "returns http success" do
+end
+      context "non-admin users" do
+      it "are not able to access the index action" do
       get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
+       expect(response).to redirect_to "/"
+       expect(flash[:alert]).to eq "You must be an admin to do that."
+     end
+   end
 end
